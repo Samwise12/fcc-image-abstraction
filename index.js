04894745261f5f4ba6eb28eb3e4f975687316c9f
Samwise12 <sfelder1@bingahmton.edu>
2016-07-31 18:11:39 -0400

    heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 var path = require('path');
 ///Config 
 var port = Number(process.env.PORT || 5000);
-/*
+
 var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/store';
 var mongoOptions = {db: {safe: true}};
 var Query = require('./model/query');
@@ -21,7 +21,7 @@ mongoose.connection.on('error', function(err){
 	console.error('MongoDB connection error: '+err);
 	process.exit(1);
 });
-*/
+
 app.use(express.static('views'));
 ////Routes
 
